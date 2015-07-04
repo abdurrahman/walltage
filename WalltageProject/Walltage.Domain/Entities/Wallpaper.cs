f6ad11c6be4947c38e3990a986355fd0db06849c
xJason21 <abdurrahman@uykusuzadam.com>
2015-07-04 09:27:07 +0300

    First commit for domain layer

---
+++ WalltageProject/Walltage.Domain/Entities/Wallpaper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Walltage.Domain.Entities
+{
+    public class Wallpaper
+    {
+        public int WallpaperId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public int ResolutionId { get; set; }
+        public string Size { get; set; }
+        public int Viewed { get; set; }
+        public int UploaderId { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Tags { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ WalltageProject/Walltage.Domain/Entities/WalltageDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using MySql.Data.Entity;
+
+namespace Walltage.Domain.Entities
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class WalltageDbContext : DbContext
+    {
+        public DbSet<Wallpaper> Wallpapers { get; set; }
+    }
+}
