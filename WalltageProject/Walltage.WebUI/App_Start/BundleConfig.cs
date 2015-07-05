3a035d009a2564cd85c7958890e26e5860f6bba5
Abdurrahman Işık <abdurrahman@uykusuzadam.com>
2015-07-05 19:29:06 +0300

    Lets use some sass :)

--- WalltageProject/Walltage.WebUI/App_Start/BundleConfig.cs
+++ WalltageProject/Walltage.WebUI/App_Start/BundleConfig.cs
@@ -9,23 +9,26 @@ namespace Walltage.WebUI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/assets/js/jquery-{version}.js"));
+                "~/assets/js/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/assets/js/jquery.validate*"));
+                "~/assets/js/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/assets/js/modernizr-*"));
+                "~/assets/js/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/assets/js/bootstrap.js",
-                      "~/assets/js/respond.js"));
+                "~/assets/js/bootstrap.js",
+                "~/assets/js/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/assets/css/bootstrap.css",
-                      "~/assets/css/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/assets/css/normalize.css",
+                "~/assets/css/bootstrap.css",
+                "~/assets/css/style.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
