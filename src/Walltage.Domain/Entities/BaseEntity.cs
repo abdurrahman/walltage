7caab1e96e44d0e2ad13f7322969c87dac25eb2f
xJason21 <datnetdeveloper@gmail.com>
2016-06-28 01:05:48 +0300

    Re-created whole project

---
+++ src/Walltage.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walltage.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ src/Walltage.Domain/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walltage.Domain
+{
+    class GenericRepository
+    {
+    }
+}
